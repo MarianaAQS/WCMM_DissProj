ffa6ca07b8e37fe6e3f790781dcf11fa3f50a626
MarianaAQS <87387919+MarianaAQS@users.noreply.github.com>
2021-07-14 12:57:28 +0100

    Update varSession.ts

--- WCMM1/src/providers/varSession.ts
+++ WCMM1/src/providers/varSession.ts
@@ -7,12 +7,13 @@ export class varSession {
 
   mensagemSMS: string = "EMERGENCY SOS! Mariana Santos triggered an assistance alert from this approximate location and may be in need of help. You are receiving this message because Mariana has listed you as an emergency contact. \n\nLocation: ";
   profileName: string;
-  profilePicture: string = "assets/Mariana_Santos.jpg";
+  profilePicture: string = "assets/person.jpg";
   firstName: string = "Mariana";
   lastName: string = "Santos";
   age: number = 22;
   sex: number = 1;
-  phone1: string = "+351962712312";
+  // insert emergency contacts
+  phone1: string = "+351...";
   phone2: string = "+351...";
   phone3: string = "+351...";
   MedName: string;
